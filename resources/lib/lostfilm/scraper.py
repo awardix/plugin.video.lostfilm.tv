6d060bec3ac2ab717168dcb3ecbb9f23ce338c95
Nemiroff <nemiroff@xaker.ru>
2019-04-11 09:20:22 +0500

    Переезд на https для старого домена.

--- resources/lib/lostfilm/scraper.py
+++ resources/lib/lostfilm/scraper.py
@@ -71,7 +71,7 @@ TorrentLink = namedtuple('TorrentLink', ['quality', 'url', 'size'])
 
 
 class LostFilmScraper(AbstractScraper):
-    BASE_URL = "http://old.lostfilm.tv"
+    BASE_URL = "https://old.lostfilm.tv"
     LOGIN_URL = "http://www.lostfilm.tv/ajaxik.php"
     BLOCKED_MESSAGE = "Контент недоступен на территории Российской Федерации"
 
